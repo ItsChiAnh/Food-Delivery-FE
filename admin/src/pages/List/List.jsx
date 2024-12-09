51d3ee0daac408d168179349dbc1f0a22c40827c
hieu <nguyentrunghieu17102k2@gmail.com>
2024-12-09 20:59:58 +0700

    edit admin panel

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -31,32 +31,30 @@ const List = ({ url }) => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <p>All Foods List</p>
-        <div className="list-table">
-          <div className="list-table-format title">
-            <b>Image</b>
-            <b>Name</b>
-            <b>Category</b>
-            <b>Price</b>
-            <b>Action</b>
-          </div>
-          {list.map((item, index) => {
-            return (
-              <div key={index} className="list-tabl-format">
-                <img src={`${url}/images/` + item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{item.category}</p>
-                <p>{item.price}</p>
-                <p onClick={() => removeFood(item._id)} className="cursor">
-                  {" "}
-                  x
-                </p>
-              </div>
-            );
-          })}
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
         </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{item.price}</p>
+              <p onClick={() => removeFood(item._id)} className="cursor">
+                {" "}
+                x
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
