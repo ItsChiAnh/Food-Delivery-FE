b08e96340e2fd1e1959f89fb2b4b687a507030cc
Hai Nhan <hainhan233@gmail.com>
2025-01-18 12:52:25 +0700

    change link url

--- food-frontend/src/context/StoreContext.jsx
+++ food-frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState("");
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-be-xk4s.onrender.com";
   const [food_list, setFoodList] = useState([]);
   const [token, setToken] = useState("");
 
